d2bf16ba0fe24078d0f36f8ebffe6c04a599c865
Mikalord <63506118+Mikalord@users.noreply.github.com>
2021-04-10 10:26:53 +0300

    Added Login Authentication/Created ReactApp
    Created various pages on client side

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port;
 
 const server = new ApolloServer({
   typeDefs,
@@ -13,9 +17,12 @@ const server = new ApolloServer({
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log("Mmemes");
-    return server.listen({ port: 5000 });
+    console.log("Mongoose Connected");
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
   });
